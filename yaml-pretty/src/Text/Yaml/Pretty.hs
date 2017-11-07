948dd5ae4d01e764d18680c935866e4a21f9c1e4
Franz Thoma <franz.thoma@tngtech.com>
2017-11-07 23:14:11 +0100

    YAML prettyprinter (incomplete)

---
+++ yaml-pretty/src/Text/Yaml/Pretty.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Text.Yaml.Pretty where
+
+import           Data.Bool                 (bool)
+import qualified Data.HashMap.Lazy         as Map
+import           Data.List                 (intersperse)
+import           Data.Scientific           (Scientific)
+import           Data.Text                 (Text)
+import qualified Data.Text                 as Text
+import           Data.Text.Prettyprint.Doc hiding (encloseSep)
+import qualified Data.Vector               as Vector
+import           Data.Yaml
+
+prettyYaml :: ToJSON a => a -> Doc ()
+prettyYaml = prettyValue . toJSON
+
+prettyValue :: Value -> Doc a
+prettyValue = \case
+  Object o -> prettyObject o
+  Array a -> prettyArray a
+  String s -> prettyString s
+  Number n -> prettyNumber n
+  Bool b -> bool "false" "true" b
+  Null -> "null"
+
+prettyObject :: Object -> Doc a
+prettyObject = render . Map.toList
+  where
+    render kvps
+        | null kvps = "{}"
+        | otherwise = encloseSep
+                          (flatAlt "" "{ ")
+                          (flatAlt "" " }")
+                          (flatAlt "" ", ")
+                          (fmap renderKeyValuePair kvps)
+    renderKeyValuePair (k, v) = pretty k <> ":" <+> group (nest 2 (flatAlt line "" <> prettyValue v))
+
+prettyArray :: Array -> Doc a
+prettyArray = render . Vector.toList
+  where
+    render vs
+        | null vs   = "[]"
+        | otherwise = encloseSep
+                          (flatAlt "- " "[ ")
+                          (flatAlt ""   " ]")
+                          (flatAlt "- " ", ")
+                          (fmap (group . nest 2 . prettyValue) vs)
+
+
+prettyString :: Text -> Doc a
+prettyString s
+    | Text.any (== ':') s  = "\"" <> pretty s <> "\""
+    | otherwise            = softQuote <> float s <> softQuote
+  where
+    softQuote = flatAlt "\"" ""
+    float = fillSep . fmap pretty . Text.words
+
+prettyNumber :: Scientific -> Doc a
+prettyNumber = undefined
+
+encloseSep :: Doc a -> Doc a -> Doc a -> [Doc a] -> Doc a
+encloseSep left right sep docs = left <> mconcat (intersperse (line' <> sep) docs) <> right
